0b00540faff7239fbe935ca627449ff930d6860b
EmerBV <emersmc@hotmail.com>
2023-07-27 23:33:42 +0200

    Initial Commit

---
+++ SwiftUber/App/SwiftUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUberApp.swift
+//  SwiftUber
+//
+//  Created by Emerson Balahan Varona on 27/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
