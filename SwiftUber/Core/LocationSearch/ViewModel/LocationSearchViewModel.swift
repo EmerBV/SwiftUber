a166dec9557fbc7ed7b2e096963c2ca97a828e6b
EmerBV <emersmc@hotmail.com>
2023-07-28 13:22:03 +0200

    + Selecting a location

---
+++ SwiftUber/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  LocationSearchViewModel.swift
+//  SwiftUber
+//
+//  Created by Emerson Balahan Varona on 27/7/23.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    
+    // MARK: - Properties
+    @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocation: String?
+    
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = "" {
+        didSet {
+            print("DEBUG: Query fragment is \(queryFragment)")
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+    
+    // MARK: - Helpers
+    func selectLocation(_ location: String) {
+        self.selectedLocation = location
+        print("DEBUG: Selected location is \(String(describing: self.selectedLocation))")
+    }
+}
+
+// MARK: - MKLocalSearchCompleterDelegate
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
--- SwiftUber/Core/LocationSearch/Views/LocationSearchView.swift
+++ SwiftUber/Core/LocationSearch/Views/LocationSearchView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct LocationSearchView: View {
     @State private var startLocationText = ""
-    @State private var destinationLocationText = ""
+    @Binding var showLocationSearchView: Bool
+    @EnvironmentObject var viewModel: LocationSearchViewModel
     
     var body: some View {
         VStack {
@@ -35,7 +36,7 @@ struct LocationSearchView: View {
                         .background(Color(.systemGroupedBackground))
                         .padding(.trailing)
                     
-                    TextField("Where to?", text: $destinationLocationText)
+                    TextField("Where to?", text: $viewModel.queryFragment)
                         .frame(height: 32)
                         .background(Color(.systemGray4))
                         .padding(.trailing)
@@ -50,8 +51,20 @@ struct LocationSearchView: View {
             // list view
             ScrollView {
                 VStack(alignment: .leading) {
+                    // Dummy data
+                    /*
                     ForEach(0 ..< 20, id: \.self) { _ in
                         LocationSearchResultCell()
+                    }
+                     */
+                    
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                viewModel
+                                    .selectLocation(result.title)
+                                showLocationSearchView.toggle()
+                            }
                     }
                 }
             }
@@ -62,6 +75,6 @@ struct LocationSearchView: View {
 
 struct LocationSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchView()
+        LocationSearchView(showLocationSearchView: .constant(false))
     }
 }
