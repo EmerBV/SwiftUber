e039dfd56f9d548d0c3dbfc815c1b62b7359f64f
EmerBV <emersmc@hotmail.com>
2023-08-27 12:33:52 +0200

    Adding trip data to ride request view

---
+++ SwiftUber/Models/UberLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UberLocation.swift
+//  SwiftUber
+//
+//  Created by Emerson Balahan Varona on 27/8/23.
+//
+
+import Foundation
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
